57ab7694e5d0108f423988cd2241f34e0ca30490
DrinMarevci <drinmarevci2002@gmail.com>
2023-10-11 04:01:09 -0700

    close modal when X button is clicked

--- components/Shared/Modal.tsx
+++ components/Shared/Modal.tsx
@@ -40,7 +40,10 @@ export default function UniversalModal({
               width={40}
               height={40}
             />
-            <button className="absolute right-2 top-2 text-gray-500 hover:text-gray-700">
+            <button
+              onClick={close}
+              className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
+            >
               <svg
                 fill="white"
                 xmlns="http://www.w3.org/2000/svg"
