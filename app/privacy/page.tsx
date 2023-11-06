3629026443075290b70993ab099fb46d726a59bb
DrinMarevci <drinmarevci2002@gmail.com>
2023-11-06 04:53:27 -0800

    Privacy Policy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,11 @@
+import PrivacyModal from '@/components/Shared/PrivacyModal'
+
+export default function PrivacyPolicy() {
+  return (
+    <main className="mt-16 flex items-center justify-center">
+      <section className="max-w-2xl">
+        <PrivacyModal />
+      </section>
+    </main>
+  )
+}
